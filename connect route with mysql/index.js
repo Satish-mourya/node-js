de397dc60317d1d0a82575f53df1961db624097b
Satish-mourya <satishmourya.o970@gmail.com>
2025-01-08 00:59:48 +0530

    added new route to update data

--- connect route with mysql/index.js
+++ connect route with mysql/index.js
@@ -5,7 +5,9 @@ uuidv4();
 const mysql=require('mysql2');
 const path=require('path');
 const app=express()
+const methodOverride=require('method-override');
 
+app.use(methodOverride("_method"));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"/views"));
 
@@ -25,7 +27,7 @@ const connection =mysql.createConnection({
 
 
  
-
+// show the no of user
 app.get('/',(req,res)=>{
     let q="select count(*) from user";
     try{
@@ -40,7 +42,54 @@ app.get('/',(req,res)=>{
     }
 })
 
-app.get("/user")
+// show the user detail
+app.get("/user",(req,res)=>{
+    let q="select * from user";
+    try {
+        connection.query(q,(err,result)=>{
+            if (err) throw err;
+            let data=result;
+             res.render("user.ejs",{data});
+        })
+    } catch (error) {
+        console.log(err);
+        res.send("some error occured");
+    }
+})
+
+// edit route form 
+app.get('/user/:id/edit',(req,res)=>{
+    let {id}=req.params;
+    console.log(id);
+    let q=`select* from user where id="${id}"`;
+    try {
+        connection.query(q,(err,result)=>{
+            if(err) throw err;
+            let data=result[0];
+            res.render("edit.ejs",{data});
+        })
+    } catch (error) {
+        console.log(err);
+        res.send("some error occured");
+    }
+})
+
+// update info
+
+app.patch('/user/:id',(req,res)=>{
+    let {id}=req.params;
+    let q=`select* from user where id="${id}"`;
+    try {
+        connection.query(q,(err,result)=>{
+            if(err) throw err;
+            let data=result[0];
+            res.send(data);
+        })
+    } catch (error) {
+        console.log(err);
+        res.send("some error occured in db ");
+    }
+})
 
 const port=5000;
 app.listen(port,()=>{
